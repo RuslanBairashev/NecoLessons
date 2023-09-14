94dec13b50681b43a60addc4993d480fd8dd3842
RuslanBairashev <ruslan.bairashev@gmail.com>
2023-09-14 18:04:44 +0300

    Jetpack Compose KOTLIN #2

--- app/src/main/java/com/ruba/necolessons/MainActivity.kt
+++ app/src/main/java/com/ruba/necolessons/MainActivity.kt
@@ -3,22 +3,48 @@ package com.ruba.necolessons
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            newText()
+            Row(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .fillMaxWidth().fillMaxHeight(0.5f)
+            ) {
+                Column(modifier = Modifier
+                    .background(Color.Red).fillMaxWidth(0.5f).fillMaxHeight(),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Hello")
+                    Text(text = "Gerona")
+                    Text(text = "Madrid")
+                }
+                Column(modifier = Modifier
+                    .background(Color.Green).fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Hello")
+                    Text(text = "Gerona")
+                    Text(text = "Madrid")
+                }
+            }
         }
     }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun newText() {
-    Text(text = "Hello 34!")
 }
