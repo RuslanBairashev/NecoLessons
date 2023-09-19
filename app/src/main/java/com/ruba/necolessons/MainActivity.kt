38d628013c740056db0ea4b74771166cba3578b3
RuslanBairashev <ruslan.bairashev@gmail.com>
2023-09-19 21:01:14 +0300

    Jetpack Compose KOTLIN #3

--- app/src/main/java/com/ruba/necolessons/MainActivity.kt
+++ app/src/main/java/com/ruba/necolessons/MainActivity.kt
@@ -3,46 +3,73 @@ package com.ruba.necolessons
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier
-                    .background(Color.Gray)
-                    .fillMaxWidth().fillMaxHeight(0.5f)
-            ) {
-                Column(modifier = Modifier
-                    .background(Color.Red).fillMaxWidth(0.5f).fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Hello")
-                    Text(text = "Gerona")
-                    Text(text = "Madrid")
-                }
-                Column(modifier = Modifier
-                    .background(Color.Green).fillMaxSize(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Hello")
-                    Text(text = "Gerona")
-                    Text(text = "Madrid")
+            Column {
+                listenItem(name = "Ryan Gosling", prof = "actor")
+                listenItem(name = "Ryan Gosling", prof = "actor")
+                listenItem(name = "Ryan Gosling", prof = "actor")
+                listenItem(name = "Ryan Gosling", prof = "actor")
+                listenItem(name = "Ryan Gosling", prof = "actor")
+                listenItem(name = "Ryan Gosling", prof = "actor")
+            }
+        }
+    }
+}
+
+@Composable
+fun listenItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Box() {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.gosl),
+                    "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column() {
+                    Text(text = name)
+                    Text(text = prof)
                 }
             }
         }
